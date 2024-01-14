3dd9b47c0e615d007b8b9ab6892a374ba0174803
ljt0815 <ljt0815@naver.com>
2024-01-14 21:40:07 +0900

    first cube

---
+++ src/MyElement3D.jsx
@@ -0,0 +1,13 @@
+﻿function MyElement3D() {
+    return (
+        <>
+            <directionalLight position={[1,1,1]} />
+            <mesh rotation={[0, 45*Math.PI/180, 0]}>
+                <boxGeometry />
+                <meshStandardMaterial color="#e67e22" />
+            </mesh>
+        </>
+    )
+}
+
+export default MyElement3D
